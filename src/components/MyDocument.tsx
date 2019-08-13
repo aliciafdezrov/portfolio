409cf5a1f90df3caac064fc30351f3c33dfc16c2
Alicia <programaciongsl2@gmail.com>
2019-08-13 11:40:27 +0200

    Added option for download cv

--- src/components/MyDocument.tsx
+++ src/components/MyDocument.tsx
@@ -1,7 +1,18 @@
 import * as React from "react";
-import { Document, Page } from 'react-pdf';
+import {Document, Page} from 'react-pdf';
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const file = require("../../docs/CV.pdf");
+const notify = () => toast('⚠️ ¿Te gustaría descargarlo? ', {
+    position: "top-right",
+    autoClose: 15000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    containerId: 'Download'
+});
 
 export class MyDocument extends React.Component<{}, {}> {
     public constructor(props) {
@@ -11,22 +22,33 @@ export class MyDocument extends React.Component<{}, {}> {
     state = {
         numPages: null,
         pageNumber: 1,
+
     };
 
-    onDocumentLoadSuccess = ({ numPages }) => {
-        this.setState({ numPages });
+    onDocumentLoadSuccess = ({numPages}) => {
+        this.setState({numPages});
+        notify();
     };
 
     public render() {
         return (
-            <div>
-                <Document
-                    file={file}
-                    onLoadSuccess={this.onDocumentLoadSuccess}
-                >
-                    <Page pageNumber={this.state.pageNumber} />
-                </Document>
-                <p>Página {this.state.pageNumber} de {this.state.numPages}</p>
+            <div className="container-fluid">
+                <div>
+                    <div>
+                        <ToastContainer containerId={'Download'}
+                                        onClick={() => {window.location=file}}
+                                        position={toast.POSITION.TOP_LEFT}/>
+                    </div>
+                </div>
+                <div>
+                    <Document
+                        file={file}
+                        onLoadSuccess={this.onDocumentLoadSuccess}
+                    >
+                        <Page pageNumber={this.state.pageNumber}/>
+                    </Document>
+                    <p>Página {this.state.pageNumber} de {this.state.numPages}</p>
+                </div>
             </div>
         );
     }
