f959775a90808cd72b7406092e4284a6551b1e9a
Alicia <programaciongsl2@gmail.com>
2019-08-13 14:58:02 +0200

    Added climbing box loader

--- src/components/MyDocument.tsx
+++ src/components/MyDocument.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import {Document, Page} from 'react-pdf';
 import {toast, ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader';
 
 const file = require("../../docs/CV.pdf");
 const notify = () => toast('⚠️ ¿Te gustaría descargarlo? ', {
@@ -22,11 +23,12 @@ export class MyDocument extends React.Component<{}, {}> {
     state = {
         numPages: null,
         pageNumber: 1,
-
+        loading: true
     };
 
     onDocumentLoadSuccess = ({numPages}) => {
         this.setState({numPages});
+        this.setState({loading: false});
         notify();
     };
 
@@ -43,6 +45,12 @@ export class MyDocument extends React.Component<{}, {}> {
                 <div>
                     <Document
                         file={file}
+                        onItemClick={this.setState({pageNumber: this.state.pageNumber+1})}
+                        error="Algo fue mal al cargar el cv. Envíame un correo al email de contacto y puedo enviartelo personalmente."
+                        loading={<ClimbingBoxLoader color={'#4DB6AC'}
+                                                    size={40}
+                                                    css="climbing-loader"
+                                                    loading={this.state.loading}/>}
                         onLoadSuccess={this.onDocumentLoadSuccess}
                     >
                         <Page pageNumber={this.state.pageNumber}/>
