7486ff9c13ce50dc9a2265fe433d720ce4584611
Alicia <programaciongsl2@gmail.com>
2019-10-01 16:01:15 +0200

    Changed technologies distribution

--- src/pages/KnowledgesPage.tsx
+++ src/pages/KnowledgesPage.tsx
@@ -19,49 +19,44 @@ export class KnowledgePage extends React.Component<{}, {}> {
                     </div>
                     <div className="blocks-container">
                         <div>
-                            <h3>Frontend</h3>
-
+                            <h3>MERN Stack</h3>
                             <TechnologyRow percent={90} label="React"/>
                             <TechnologyRow percent={80} label="Redux"/>
                             <TechnologyRow percent={75} label="Webpack"/>
-                            <TechnologyRow percent={65} label="Morphia"/>
-                            <TechnologyRow percent={75} label="Jest"/>
-                            <TechnologyRow percent={70} label="Enzyme"/>
+                            <TechnologyRow percent={80} label="NodeJS"/>
+                            <TechnologyRow percent={90} label="Jest"/>
+                            <TechnologyRow percent={90} label="ES6"/>
+                            <TechnologyRow percent={75} label="Express"/>
+                            <TechnologyRow percent={75} label="Nginx"/>
                         </div>
 
                         <div>
-                            <h3>Backend</h3>
+                            <h3>J2EE Stack</h3>
                             <TechnologyRow percent={95} label="J2EE"/>
                             <TechnologyRow percent={95} label="Java"/>
-                            <TechnologyRow percent={70} label="Apache Spark"/>
-                            <TechnologyRow percent={45} label="GraphQL"/>
-                            <TechnologyRow percent={95} label="RestFull"/>
-                            <TechnologyRow percent={80} label="NodeJS"/>
                             <TechnologyRow percent={80} label="JUnit"/>
                             <TechnologyRow percent={80} label="Mockito"/>
+                            <TechnologyRow percent={90} label="Maven"/>
+                            <TechnologyRow percent={90} label="Wildfly"/>
                         </div>
 
                         <div>
-                            <h3>Bases de datos</h3>
-
-                            <TechnologyRow percent={90} label="MySQL"/>
-                            <TechnologyRow percent={70} label="Oracle"/>
-                            <TechnologyRow percent={90} label="MongoDB"/>
-                        </div>
-
-                        <div>
-                            <h3>Otros</h3>
-
-                            <TechnologyRow percent={100} label="Git"/>
+                            <h3>Devops</h3>
                             <TechnologyRow percent={80} label="Docker"/>
                             <TechnologyRow percent={80} label="Docker Compose"/>
                             <TechnologyRow percent={80} label="Docker Swarm"/>
                             <TechnologyRow percent={80} label="Jenkins"/>
-                            <TechnologyRow percent={90} label="ES6"/>
-                            <TechnologyRow percent={75} label="Express"/>
-                            <TechnologyRow percent={90} label="Maven"/>
-                            <TechnologyRow percent={90} label="Wildfly"/>
-                            <TechnologyRow percent={75} label="Nginx"/>
+                        </div>
+
+                        <div>
+                            <h3>Otros</h3>
+                            <TechnologyRow percent={45} label="GraphQL"/>
+                            <TechnologyRow percent={95} label="RestFull"/>
+                            <TechnologyRow percent={100} label="Git"/>
+                            <TechnologyRow percent={90} label="MySQL"/>
+                            <TechnologyRow percent={70} label="Oracle"/>
+                            <TechnologyRow percent={90} label="MongoDB"/>
+                            <TechnologyRow percent={90} label="Clean Code"/>
                         </div>
                     </div>
                 </Paper>
