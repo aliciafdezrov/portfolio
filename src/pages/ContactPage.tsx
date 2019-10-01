bf54112b210cb4218fc0aaebc5f9246514e7f1e4
Alicia <programaciongsl2@gmail.com>
2019-10-01 16:16:52 +0200

    Added about me section on contact page

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -7,7 +7,7 @@ import FontIcon from "react-md/lib/FontIcons";
 import {Divider} from "react-md/lib/Dividers";
 import {BrowserView, MobileView} from 'react-device-detect';
 
-const photo = require("../../assets/avatars/photo.jpg");
+const photo = require("../../assets/avatars/repository-open-graph-template.png")//require("../../assets/avatars/photo.jpg");
 
 export interface IPropsContactPage {
 }
@@ -33,60 +33,122 @@ export class ContactPage extends React.Component<IPropsContactPage & IDispatchPr
 
     public render() {
         return (
-            <div className="central-div">
-                <Paper className="title-paper white-background contact-page-container" zDepth={2}>
-                    <div>
-                        <div className="contact-title contact-text-div">
-                            <h4>Información de contacto</h4>
-                            <Divider/>
-                        </div>
+            <>
+                <BrowserView>
+                    <div className="central-div">
+                        <Paper className="title-paper white-background contact-page-container" zDepth={2}>
+                            <div>
+                                <>
+                                    <div className="contact-title contact-text-div">
+                                        <h4>Información de contacto</h4>
+                                        <Divider/>
+                                    </div>
 
-                        <div className="contact-title contact-list">
-                            <List>
-                                <div>
-                                    <BrowserView>
-                                        <ListItem primaryText="aliciafdezrov@gmail.com"
-                                                  onClick={() => this.openLink("https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&su=Petición%20desde%20cv&to=aliciafdezrov@gmail.com&body=Cuerpo+del+mensaje")}
-                                                  leftIcon={<FontIcon>mail</FontIcon>}>
-                                        </ListItem>
-                                    </BrowserView>
-                                    <MobileView>
+                                    <div className="contact-title contact-list">
+                                        <List>
+                                            <ListItem primaryText="aliciafdezrov@gmail.com"
+                                                      onClick={() => this.openLink("https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&su=Petición%20desde%20cv&to=aliciafdezrov@gmail.com&body=Cuerpo+del+mensaje")}
+                                                      leftIcon={<FontIcon>mail</FontIcon>}>
+                                            </ListItem>
+
+                                            <ListItem primaryText="Málaga, 29012 (Spain)"
+                                                      leftIcon={<FontIcon>place</FontIcon>}>
+                                            </ListItem>
+                                        </List>
+
+                                        <div style={{paddingTop: '2vh'}}>
+                                            <a href="https://www.linkedin.com/in/alicia-fernández-rovira-70885811a"
+                                               target="_blank">
+                                                https://www.linkedin.com/in/alicia-fernández-rovira-70885811a
+                                            </a>
+                                        </div>
+                                        <div style={{paddingTop: '2vh'}}>
+                                            <a href="https://github.com/aliciafdezrov"
+                                               target="_blank">
+                                                https://github.com/aliciafdezrov
+                                            </a>
+                                        </div>
+                                    </div>
+                                </>
+
+                                <>
+                                    <div>
+                                        <h4>Un poco más sobre mi</h4>
+                                        <Divider/>
+                                        <p>Llevo desde 2016 trabajando, primero con una beca compaginándolo con mis
+                                            estudios y
+                                            más tarde contratada.Desde ese momento he estado trabajando como
+                                            Desarrolladora
+                                            Full-Stack. Siempre participando activamente en el diseño, creación y
+                                            gestión de
+                                            bases de datos, backend y frontend.</p>
+                                    </div>
+                                </>
+                            </div>
+
+                            <div className="avatar-style">
+                                <Avatar
+                                    alt="Me"
+                                    src={photo}
+                                    className="avatar"
+                                />
+                            </div>
+                        </Paper>
+                    </div>
+                </BrowserView>
+                <MobileView>
+                    <div className="central-div">
+                        <Paper className="title-paper white-background contact-page-container" zDepth={2}>
+                            <div>
+                                <div className="contact-title contact-text-div">
+                                    <h4>Información de contacto</h4>
+                                    <Divider/>
+                                </div>
+
+                                <div className="contact-title contact-list">
+                                    <List>
                                         <ListItem primaryText="aliciafdezrov@gmail.com"
                                                   onClick={() => this.openLink("mailto:aliciafdezrov@gmail.com?subject=Petición%20desde%20cv")}
                                                   leftIcon={<FontIcon>mail</FontIcon>}>
                                         </ListItem>
-                                    </MobileView>
+                                    </List>
+
+                                    <div style={{paddingTop: '1vh'}}>
+                                        <a href="https://www.linkedin.com/in/alicia-fernández-rovira-70885811a"
+                                           target="_blank">
+                                            linkedin/alicia-fernández-rovira
+                                        </a>
+                                    </div>
+                                    <div style={{paddingTop: '1vh'}}>
+                                        <a href="https://github.com/aliciafdezrov"
+                                           target="_blank">
+                                            github.com/aliciafdezrov
+                                        </a>
+                                    </div>
                                 </div>
 
-                                <ListItem primaryText="Málaga, 29012 (Spain)"
-                                          leftIcon={<FontIcon>place</FontIcon>}>
-                                </ListItem>
-                            </List>
-
-                            <div style={{paddingTop: '2vh'}}>
-                                <a href="https://www.linkedin.com/in/alicia-fernández-rovira-70885811a"
-                                   target="_blank">
-                                    https://www.linkedin.com/in/alicia-fernández-rovira-70885811a
-                                </a>
+                                <div className="contact-title contact-text-div">
+                                    <h4>Un poco más sobre mi</h4>
+                                    <Divider/>
+                                    <p>Llevo desde 2016 trabajando, primero con una beca compaginándolo con mis estudios
+                                        y
+                                        más tarde contratada.Desde ese momento he estado trabajando como Desarrolladora
+                                        Full-Stack. Siempre participando activamente en el diseño, creación y gestión de
+                                        bases de datos, backend y frontend.</p>
+                                </div>
                             </div>
-                            <div style={{paddingTop: '2vh'}}>
-                                <a href="https://github.com/aliciafdezrov"
-                                   target="_blank">
-                                    https://github.com/aliciafdezrov
-                                </a>
-                            </div>
-                        </div>
-                    </div>
 
-                    <div className="avatar-style">
-                        <Avatar
-                            alt="Me"
-                            src={photo}
-                            className="avatar"
-                        />
+                            <div className="avatar-style">
+                                <Avatar
+                                    alt="Me"
+                                    src={photo}
+                                    className="avatar"
+                                />
+                            </div>
+                        </Paper>
                     </div>
-                </Paper>
-            </div>
+                </MobileView>
+            </>
         );
     }
 }
