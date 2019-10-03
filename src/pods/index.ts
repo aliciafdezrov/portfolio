99ba37a4f84cc05c5452711bcd9f9f1aa4892e7d
Alicia <programaciongsl2@gmail.com>
2019-10-03 10:09:33 +0200

    Deleted react-md package and dashboard pod with navbar

--- src/pods/index.ts
+++ src/pods/index.ts
@@ -1,7 +1,6 @@
 export * from './academic';
 export * from './contact';
 export * from './experience';
-export * from './dashboard';
 export * from './home';
 export * from './knowledges';
 export * from './pdf.page';
--- src/scenes/main.scene.tsx
+++ src/scenes/main.scene.tsx
@@ -3,8 +3,6 @@ import {useState} from "react";
 import {AcademicPage, ContactPage, ExperiencePage, HomePage, KnowledgesPage, PDF} from "pods";
 import {SwitchComponent} from "../components/switch.component";
 
-const themes = ['home', 'academic', 'experience', 'knowledge', 'contact', 'cv'];
-
 export const MainScene = () => {
     const [activeIndex, setActiveIndex] = useState(5);
     const [transition, setTransition] = useState('slide-forward');
@@ -31,29 +29,6 @@ export const MainScene = () => {
                 <ContactPage key="contact"/>,
                 <PDF key="cv"/>
             ]} index={activeIndex} transition={transition}/>
-
-            {/*<DashboardPage theme={`bottom-navigations-dynamic--${themes[activeIndex]}`}
-                           activeIndex={activeIndex}
-                           links={[{
-                               label: 'Home',
-                               icon: <FontIcon>home</FontIcon>,
-                           }, {
-                               label: 'Académico',
-                               icon: <FontIcon>school</FontIcon>,
-                           }, {
-                               label: 'Experiencia',
-                               icon: <FontIcon>laptop</FontIcon>,
-                           }, {
-                               label: 'Tecnologías',
-                               icon: <FontIcon>star_border</FontIcon>,
-                           }, {
-                               label: 'Sobre mi',
-                               icon: <FontIcon>person_outline</FontIcon>,
-                           }, {
-                               label: 'Descarga',
-                               icon: <FontIcon>cloud_download</FontIcon>,
-                           }]}
-                           setActiveIndex={onChangeIndex.bind(this)}/>*/}
         </>
     );
 }
