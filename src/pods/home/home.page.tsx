ca9d16ec07b360953661c4bfb155d88d0aceb7c8
Alicia <programaciongsl2@gmail.com>
2019-10-04 08:37:47 +0200

    Added effects at title on home page

--- src/pods/home/home.page.tsx
+++ src/pods/home/home.page.tsx
@@ -1,19 +1,45 @@
 import * as React from "react";
 import * as classes from './home.scss';
+import { Parallax } from 'react-parallax';
+import { AnimatedTitle } from 'react-genie';
 
 export const HomePage = () => {
-    return (
-        <>
-            <div className={classes.wrapper}>
-                <div className={classes.slide}/>
+	return (
+		<>
+			<Parallax strength={-200}
+								blur={10}
+								bgImage={require("../../../assets/ash-edmonds-510566-unsplash-min.jpg")}
+								bgImageAlt="the cat">
+				<div className={classes.wrapper}>
+					<div className={classes.slide}/>
 
-                <div className={classes.card}>
-                    <div className={classes.title}>
-                        <h1>Alicia Fernández Rovira</h1>
-                        <h4>Desarrolladora Full-Stack en Málaga</h4>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+					<div className={classes.card}>
+						<div className={classes.title}>
+							<AnimatedTitle style={{
+								color: 'white',
+								fontWeight: 'bold',
+								fontSize: '2em',
+								marginTop: '0.67em',
+								marginBottom: '0.67em',
+								marginLeft: '0',
+								marginRight: '0'
+							}}>
+								Alicia Fernández Rovira
+							</AnimatedTitle>
+							<AnimatedTitle
+								style={{
+									color: 'white',
+									fontWeight: 'bold',
+									fontSize: '1.2em',
+									marginTop: '1.33em',
+									marginBottom: '1.33em',
+									marginLeft: '0',
+									marginRight: '0'
+								}}>Desarrolladora Full-Stack en Málaga</AnimatedTitle>
+						</div>
+					</div>
+				</div>
+			</Parallax>
+		</>
+	);
 }
