032e0f46d023038ba83d7f003ea172ec3fdec2d8
Alicia <programaciongsl2@gmail.com>
2019-10-07 09:46:52 +0200

    Removed load progress component effect

--- src/components/progress.component.tsx
+++ src/components/progress.component.tsx
@@ -6,41 +6,35 @@ import * as chroma from "chroma-js";
 import {BrowserView, MobileView} from "react-device-detect";
 
 export interface IProgressComponentProps {
-    percent: number
+    percent: number;
 }
 
 export const ProgressComponent = (props: IProgressComponentProps) => {
-    const [percent, setPercent] = useState(0);
     const [color, setColor] = useState('#108ee9');
 
     useEffect(() => {
-        increase()
-    }, [percent]);
-
-    function increase() {
-        console.log("INCREASEEEE")
-        let newPercent = percent + 4;
         let scale = chroma.scale(['#108ee9', '#bc7bd4']);
-        setColor(scale(percent / 100).hex());
-        if (newPercent < props.percent) {
-            setPercent(newPercent);
-        }
-    }
+        setColor(scale(props.percent / 100).hex());
+    }, [props.percent]);
 
     return (
         <div>
             <BrowserView>
-                <Line percent={percent}
+                <Line percent={props.percent}
                       strokeLinecap="round"
                       strokeWidth={2}
                       strokeColor={color}/>
             </BrowserView>
             <MobileView>
-                <Line percent={percent}
+                <Line percent={props.percent}
                       strokeLinecap="round"
                       strokeWidth={2}
                       strokeColor={color}/>
             </MobileView>
         </div>
     );
-}
+};
+
+ProgressComponent.defaultProps = {
+    loadLineProgress: false
+};
