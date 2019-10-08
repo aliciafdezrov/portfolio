0e56b0c2a868a7d57f1425a6a1f42e8f4d531fe4
Alicia <programaciongsl2@gmail.com>
2019-10-08 12:22:04 +0200

    Changed effects for titles on home pod

--- src/pods/home/home.page.tsx
+++ src/pods/home/home.page.tsx
@@ -1,27 +1,30 @@
 import * as React from "react";
 import * as classes from './home.scss';
 import {Parallax} from 'react-parallax';
-import {AnimatedTitle} from 'react-genie';
+import {AnimatedTitle, Animation, Reveal} from 'react-genie';
 
 export const HomePage = () => {
-	return (
-		<>
-			<Parallax strength={-200}
-								blur={10}
-								bgImage={require("../../../assets/ash-edmonds-510566-unsplash-min.jpg")}
-								bgImageAlt="...">
-					<div className={classes.card}>
-						<div className={classes.title}>
-							<AnimatedTitle className={classes.titleText}>
+    return (
+        <>
+            <Parallax strength={-200}
+                      blur={10}
+                      bgImage={require("../../../assets/ash-edmonds-510566-unsplash-min.jpg")}
+                      bgImageAlt="...">
+                <div className={classes.card}>
+                    <div className={classes.title}>
+                        <Reveal animation={Animation.FadeIn}>
+                            <div className={classes.titleText}>
                                 Alicia Fernández Rovira
-							</AnimatedTitle>
-							<AnimatedTitle
-								className={classes.subtitleText}>
-                                Desarrolladora Full-Stack en Málaga
-							</AnimatedTitle>
-						</div>
-					</div>
-			</Parallax>
-		</>
-	);
+                            </div>
+                        </Reveal>
+                        <Reveal animation={Animation.FadeIn}>
+                           <div className={classes.subtitleText}>
+                            Desarrolladora Full-Stack en Málaga
+                           </div>
+                        </Reveal>
+                    </div>
+                </div>
+            </Parallax>
+        </>
+    );
 }
