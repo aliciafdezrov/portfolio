3e49b8f83778fa969dbe3988c0446534e65a9ed4
alicia <aliciafdezrov@gmail.com>
2021-10-17 14:39:43 +0200

    Clean up libraries and remove pdf visualization

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
-export * from './my-document/my-document.component';
 export * from './progress.component';
 export * from './technology-row';
--- src/pods/pdf/pdf.page.tsx
+++ src/pods/pdf/pdf.page.tsx
@@ -1,20 +1,49 @@
 import * as React from "react";
-import {MyDocument} from "components";
 import IconButton from "rsuite/IconButton";
 import FileDownloadIcon from '@rsuite/icons/FileDownload';
 import classes from "./pdf.scss";
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const cv = require('../../../assets/CV.pdf');
+
+const notify = () => toast('⚠️ ¿Te gustaría descargarlo? ', {
+    position: "top-right",
+    hideProgressBar: false,
+    autoClose: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    containerId: 'Download'
+});
 
 export interface Props {
     showDialog: boolean;
 }
 
 export const PDF = (props: Props) => {
+    React.useEffect(() => {
+        if (props.showDialog) {
+            notify();
+        }
+    }, [props.showDialog]);
+
+    function downloadFile() {
+        let link = document.createElement('a');
+        link.href = cv;
+        link.download = "aliciafdezrovCV";
+        link.click();
+    }
+
     return (
         <>
-            <MyDocument key="cv" showDialog={props.showDialog}/>
-            <div className={classes.mobileButtonWrapper}>
+            <div>
+                <ToastContainer containerId={'Download'}
+                                toastClassName={classes.toast}
+                                onClick={() => downloadFile()}
+                                position={toast.POSITION.TOP_LEFT}/>
+            </div>
+            <div className={classes.buttonWrapper}>
                 <IconButton color="violet" href={cv} icon={<FileDownloadIcon/>}>
                     ¡Pulsa para descargar el cv en pdf!
                 </IconButton>
