238bdcc3a18f729de7859603daaf23c4dc116e9f
alicia <aliciafdezrov@gmail.com>
2022-03-02 22:41:44 +0100

    Remove some ternary skills

--- src/pods/skills/skills.page.tsx
+++ src/pods/skills/skills.page.tsx
@@ -8,56 +8,18 @@ import classes from "./skills.scss";
 export const SkillsPage = () => (
     <div className={classes.container}>
         <div className={classes.card}>
-            <h4>Tecnologías y herramientas</h4>
+            <h4>Main Skills</h4>
             <Divider/>
 
             <div className={classes.blocksContainer}>
                 <Reveal delay={100} animation={Animation.FadeInUp}>
                     <div>
-                        <h3>MERN Stack</h3>
                         <TechnologyRow percent={90} label="React"/>
                         <TechnologyRow percent={80} label="Redux"/>
                         <TechnologyRow percent={75} label="Webpack"/>
-                        <TechnologyRow percent={80} label="NodeJS"/>
+                        <TechnologyRow percent={80} label="Cypress"/>
                         <TechnologyRow percent={90} label="Jest"/>
                         <TechnologyRow percent={90} label="ES9"/>
-                        <TechnologyRow percent={75} label="Express"/>
-                        <TechnologyRow percent={75} label="Nginx"/>
-                    </div>
-                </Reveal>
-
-                <Reveal delay={200} animation={Animation.FadeInUp}>
-                    <div>
-                        <h3>J2EE Stack</h3>
-                        <TechnologyRow percent={95} label="J2EE"/>
-                        <TechnologyRow percent={95} label="Java"/>
-                        <TechnologyRow percent={80} label="JUnit"/>
-                        <TechnologyRow percent={80} label="Mockito"/>
-                        <TechnologyRow percent={90} label="Maven"/>
-                        <TechnologyRow percent={90} label="Wildfly"/>
-                    </div>
-                </Reveal>
-
-                <Reveal delay={300} animation={Animation.FadeInUp}>
-                    <div>
-                        <h3>Devops</h3>
-                        <TechnologyRow percent={80} label="Docker"/>
-                        <TechnologyRow percent={80} label="Docker Compose"/>
-                        <TechnologyRow percent={80} label="Docker Swarm"/>
-                        <TechnologyRow percent={80} label="Jenkins"/>
-                    </div>
-                </Reveal>
-
-                <Reveal delay={400} animation={Animation.FadeInUp}>
-                    <div>
-                        <h3>Otros</h3>
-                        <TechnologyRow percent={45} label="GraphQL"/>
-                        <TechnologyRow percent={95} label="RestFull"/>
-                        <TechnologyRow percent={100} label="Git"/>
-                        <TechnologyRow percent={90} label="MySQL"/>
-                        <TechnologyRow percent={70} label="Oracle"/>
-                        <TechnologyRow percent={90} label="MongoDB"/>
-                        <TechnologyRow percent={90} label="Clean Code"/>
                     </div>
                 </Reveal>
             </div>
