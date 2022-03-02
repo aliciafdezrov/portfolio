13b5a572f67ad51dfb4396965953eb0fec9fe3e6
alicia <aliciafdezrov@gmail.com>
2022-03-02 22:41:02 +0100

    Remove chroma-js package

--- src/components/progress.component.tsx
+++ src/components/progress.component.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Progress from 'rsuite/Progress';
 import "rsuite/Progress/styles/index.less";
-import * as chroma from "chroma-js";
 
 const Line = Progress.Line;
 
@@ -13,8 +12,11 @@ export const ProgressComponent = (props: IProgressComponentProps) => {
     const [color, setColor] = React.useState('#108ee9');
 
     React.useEffect(() => {
-        let scale = chroma.scale(['#108ee9', '#bc7bd4']);
-        setColor(scale(props.percent / 100).hex());
+        if (props.percent < 50) {
+            setColor('#108ee9')
+        } else {
+            setColor('#bc7bd4')
+        }
     }, [props.percent]);
 
     return (
